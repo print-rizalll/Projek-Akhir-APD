927c7b2c13878e25c9701385dc249bd212a86b62
Az Zuhri Al Atsary <azzuhrialatsary@gmail.com>
2025-11-11 14:54:38 +0800

    data

---
+++ Data.py
@@ -0,0 +1,18 @@
+user_data = {
+    "apoteker": {"password": "1234", "role": "apoteker"},
+    "user": {"password": "2345", "role": "user"}
+}
+
+obat_data = {
+    101: {"nama": "Paracetamol", "stok": 50, "harga": 5000},
+    102: {"nama": "Amoxicillin", "stok": 30, "harga": 15000},
+    103: {"nama": "Komix", "stok": 20, "harga": 10000},
+    104: {"nama": "OBH", "stok": 15, "harga": 13000},
+    105: {"nama": "Tremenza", "stok": 20, "harga": 20000},
+    106: {"nama": "Omeprazol", "stok": 13, "harga": 15000}
+}
+
+next_kode_obat = 107
+pengguna = None
+role_pengguna = None
+run = True
